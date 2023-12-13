793ad3b5940ccdf2f39e0c4aeb36433ff27d4f8b
Carlos Victor <yvelios001@gmail.com>
2023-12-13 19:56:39 -0300

    metodo get caminhao

--- src/main/java/com/example/caminhao/controller/CaminhaoController.java
+++ src/main/java/com/example/caminhao/controller/CaminhaoController.java
@@ -1,8 +1,11 @@
 package com.example.caminhao.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,4 +26,19 @@ public class CaminhaoController {
         Caminhao caminhaoSalvo = caminhaoRepository.save(caminhao);
         return new ResponseEntity<>(caminhaoSalvo, HttpStatus.CREATED);
     }
+    
+    @GetMapping("/pegar")
+    public ResponseEntity<List<Caminhao>> getAllCaminhoes() {
+        List<Caminhao> caminhoes = caminhaoRepository.findAll();
+        if (caminhoes.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(caminhoes, HttpStatus.OK);
+    }
+    
+    
+    
+    
+    
+    
 }
